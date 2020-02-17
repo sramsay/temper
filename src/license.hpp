d547f04c0444945450d75a27d78e07cff8a19188
Stephen Ramsay <sramsay@protonmail.com>
2020-02-17 10:47:42 -0600

    Minor optimization.

--- src/license.hpp
+++ src/license.hpp
@@ -1,28 +1,3 @@
-/*
- * license.hpp
- *
- * This file is part of temper.  It contains the headers for license.cpp
- *
- * Written and maintained by Stephen Ramsay <sramsay@protonmail.com>
- *
- * Last Modified: Sun Feb 09 07:16:59 CST 2020
- *
- * Copyright © 2020 Stephen Ramsay
- *
- * temper is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3, or (at your option) any
- * later version.
- *
- * temper is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with temper; see the file COPYING.  If not see
- * <http://www.gnu.org/licenses/>.
- */
 
 #ifndef LICENSE_INCLUDED
 #define LICENSE_INCLUDED
@@ -35,4 +10,24 @@ constexpr auto DATES{"2019-2020"};
 
 std::string printVersion();
 
+inline std::string printVersion() {
+	std::stringstream ss;
+
+  ss << PROGRAM << " " << VERSION << "\n";
+  ss << "Copyright © " << DATES << " by Stephen Ramsay"
+     << "\n";
+  ss << "This program comes with ABSOLUTELY NO WARRANTY."
+     << "\n";
+  ss << "This is free software, and you are welcome to redistribute it"
+     << "\n";
+  ss << "under the terms of the GNU General Public License version 3"
+     << "\n";
+  ss << "or (at your option) any later version.  See COPYING for details,"
+     << "\n";
+  ss << "or visit <https://www.gnu.org/licenses/gpl.txt>." << std::endl;
+
+  return ss.str();
+}
+
 #endif
+
