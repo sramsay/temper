0aecfbfe016058ff97c7b6e5773824a2ac30f3ac
Stephen Ramsay <sramsay@protonmail.com>
2020-02-19 15:02:11 -0600

    Some refactoring and optimizations.

--- src/temper.cpp
+++ src/temper.cpp
@@ -1,3 +1,5 @@
+#include "conversions.hpp"
+
 #include <boost/lexical_cast/bad_lexical_cast.hpp>
 #include <boost/lexical_cast/try_lexical_convert.hpp>
 #include <boost/program_options.hpp>
@@ -28,7 +30,7 @@ int main(int argc, char *argv[]) {
         "version", "Print the version number");
 
     bpo::options_description hidden("Hidden options");
-    hidden.add_options()("input-value", bpo::value<float>(), "input value");
+    hidden.add_options()("input-value", bpo::value<double>(), "input value");
 
     bpo::options_description cmdline_options;
     cmdline_options.add(desc).add(hidden);
@@ -56,17 +58,11 @@ int main(int argc, char *argv[]) {
     }
 
     if (vm.count("input-value") != 0) {
-      constexpr auto c1{32};
-      constexpr auto c2{5.0};
-      constexpr auto c3{9.0};
-
-      const auto rawTemp{vm["input-value"].as<float>()};
-      auto cTemp{(rawTemp - c1) * c2 / c3};
-      auto fTemp{rawTemp * c3 / c2 + c1};
-      cout << fixed << setprecision(1) << rawTemp << "\u00B0F is " << cTemp
-           << "\u00B0C" << endl;
-      cout << fixed << setprecision(1) << rawTemp << "\u00B0C is " << fixed
-           << setprecision(1) << fTemp << "\u00B0F" << endl;
+      const auto input{vm["input-value"].as<double>()};
+      auto c{cTemp(input)};
+      auto f{fTemp(input)};
+      cout << fixed << setprecision(1) << input << "\u00B0F is " << c << "\u00B0C" << endl;
+      cout << fixed << setprecision(1) << input << "\u00B0C is " << f << "\u00B0F" << endl;
     } else {
       cout << desc << endl;
       exit(0);
