b03047426f952744358e45ae6b011164ed0b0c77
Stephen Ramsay <sramsay@protonmail.com>
2020-02-20 17:34:15 -0600

    Trying out lgtm.

--- test/scripts/run-tests.sh
+++ test/scripts/run-tests.sh
@@ -1,13 +1,3 @@
 #!/bin/sh 
 
-if [ -d build ]
-then
-  meson --reconfigure build
-  ninja -C build
-else 
-  meson build
-  ninja -C build
-fi
-
-./build/temper-test
-./scripts/bats ./scripts/temper_cli_tests
+scripts/bats scripts/temper_cli_tests
