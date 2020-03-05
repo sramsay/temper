255f3d046c537b9daeda2d46b04d3427c761079f
Stephen Ramsay <sramsay@protonmail.com>
2020-03-05 09:52:28 -0600

    ...

--- src/conversions.hpp
+++ src/conversions.hpp
@@ -4,18 +4,4 @@
 double cTemp(double rawTemp);
 double fTemp(double rawTemp);
 
-constexpr auto c1{32};
-constexpr auto c2{5.0};
-constexpr auto c3{9.0};
-
-inline double cTemp(double rawTemp) {
-  auto cTemp{(rawTemp - c1) * c2 / c3};
-  return cTemp;
-}
-
-inline double fTemp(double rawTemp) {
-  auto fTemp{rawTemp * c3 / c2 + c1};
-  return fTemp;
-}
-
 #endif
