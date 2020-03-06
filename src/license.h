cfc0fa0cd106f1d6aa46f79e0bf040a0dfba7dbf
Stephen Ramsay <sramsay@protonmail.com>
2020-03-06 15:30:04 -0600

    Typo.

--- src/license.h
+++ src/license.h
@@ -13,7 +13,7 @@ void print_version() {
   printf("%s %s\n", PROGRAM, VERSION);
   printf("Copyright © %s by Stephen Ramsay\n", DATES);
   printf("License GPVv3+: GNU GPL version 3 or later <https://gnu.org/licenses.gpl.html>.\n");
-  printf("This is free software: you are free to change an redistribute it.\n");
+  printf("This is free software: you are free to change and redistribute it.\n");
   printf("There is NO WARRANTY, to the extent permitted by law.\n");
 }
 
