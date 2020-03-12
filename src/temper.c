08b7745002b714fa8c8c5f47b332ed6f8e7f6849
Stephen Ramsay <sramsay@protonmail.com>
2020-03-12 10:59:29 -0500

    A much more sane algorithm for dealing with negative numbers.

--- src/temper.c
+++ src/temper.c
@@ -2,11 +2,12 @@
 #include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "conversions.h"
 #include "license.h"
 
-void print_output(double raw_temp, double f, double c, int precision);
+void print_output(double raw_temp, double f, double c, unsigned precision);
 void print_usage(void);
 
 int main(int argc, char* argv[argc + 1]) {
@@ -15,8 +16,16 @@ int main(int argc, char* argv[argc + 1]) {
     exit(EXIT_SUCCESS);
   }
 
-  regex_t regex = { 0 };
-  int regex_return_flag = 0;
+  /* The fundamental problem is that when getopt sees something like -42, it
+   * thinks that's a switch.  Worse, it thinks it's two switches (-4 and -2).
+   * So basically, we're going to take anything that looks like a temperature
+   * out of argv.  To be safe, we're going to make a copy of argv without the
+   * temperature and let getopt process that.
+   */
+  char* args[argc];
+
+  regex_t regex = {0};
+  signed regex_return_flag = 0;
   double raw_temp = 0;
 
   regex_return_flag = regcomp(&regex, "^[0-9-]+[.]?[0-9]*$", REG_EXTENDED);
@@ -25,31 +34,40 @@ int main(int argc, char* argv[argc + 1]) {
     exit(EXIT_FAILURE);
   }
 
-  regex_return_flag = regexec(&regex, argv[argc - 1], 0, (void*)0, 0);
-  if (!regex_return_flag) {  // 0 indicates successful match
-    char* end = (void*)0;
-    raw_temp = strtod(argv[argc - 1], &end);
-    if (*end) {
-      printf("Could not parse %s", argv[argc - 1]);
-      exit(EXIT_FAILURE);
+  for (int i = 0; i < argc; ++i) {
+    // Check to see if it's a temperature-like thing
+    regex_return_flag = regexec(&regex, argv[i], 0, (void*)0, 0);
+    if (!regex_return_flag) {  // 0 indicates successful match
+      // Okay, but is it actually an opt arg?
+      if (strcmp(argv[i - 1], "--precision") == 0 ||
+          strcmp(argv[i - 1], "-p") == 0) {
+        args[i] = argv[i];
+      } else {
+        char* end = (void*)0;
+        raw_temp = strtod(argv[i], &end);
+        if (*end) {
+          printf("Could not parse %s", argv[i]);
+          exit(EXIT_FAILURE);
+        }
+      }
     } else {
-      argc--;
+      args[i] = argv[i];
     }
   }
 
   regfree(&regex);
 
   static struct option long_options[] = {
-      {"help", no_argument, (void *)0, 'h'},
-      {"version", no_argument, (void *)0, 'V'},
-      {"precision", required_argument, (void *)0, 'p'},
+      {"help", no_argument, (void*)0, 'h'},
+      {"version", no_argument, (void*)0, 'V'},
+      {"precision", required_argument, (void*)0, 'p'},
       {0, 0, 0, 0}};
 
-  int opt = 0;
-  int option_index = 0;
-  int precision = 1;
+  signed opt = 0;
+  signed option_index = 0;
+  unsigned precision = 1;
 
-  while ((opt = getopt_long_only(argc, argv, "hVp:", &long_options[0],
+  while ((opt = getopt_long_only(argc, args, "hVp:", &long_options[0],
                                  &option_index)) != -1) {
     switch (opt) {
       case '?':
@@ -60,9 +78,15 @@ int main(int argc, char* argv[argc + 1]) {
       case 'h':
         print_usage();
         exit(EXIT_SUCCESS);
-      case 'p':
-        precision = atoi(optarg);
+      case 'p': {
+        char* end = (void*)0;
+        precision = strtol(optarg, &end, 10);
+        if (*end) {
+          printf("Could not parse %s", optarg);
+          exit(EXIT_FAILURE);
+        }
         break;
+      }
       default:
         print_usage();
         exit(EXIT_SUCCESS);
@@ -81,7 +105,7 @@ int main(int argc, char* argv[argc + 1]) {
   return EXIT_SUCCESS;
 }
 
-void print_output(double raw_temp, double f, double c, int precision) {
+void print_output(double raw_temp, double f, double c, unsigned precision) {
   printf("%.*f\u00B0F is %.*f\u00B0C\n", precision, raw_temp, precision, c);
   printf("%.*f\u00B0C is %.*f\u00B0F\n", precision, raw_temp, precision, f);
 }
